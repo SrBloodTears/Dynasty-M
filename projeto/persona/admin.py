ee28b65ab35a11c64161bd8f64a309bed5fa01ce
ACN-Destroyer <icefirethunderlight@gmail.com>
2024-11-13 14:59:52 -0400

    13 / 11 antes da aula

--- projeto/persona/admin.py
+++ projeto/persona/admin.py
@@ -1,9 +1,20 @@
-# persona/admin.py
 from django.contrib import admin
-from persona.models import Personagem  # Importa o modelo Personagem
+from persona.models import Personagem, Poder  # Importa o modelo Personagem e Poder
 
 @admin.register(Personagem)
 class PersonagemAdmin(admin.ModelAdmin):
     list_display = ('nome', 'grupo', 'raça', 'alinhamento')  # Colunas exibidas no admin
-    search_fields = ('nome',)  # Adiciona uma barra de pesquisa pelo nome
+    search_fields = ('nome',)  # Barra de pesquisa pelo nome
     list_filter = ('grupo', 'raça', 'alinhamento')  # Filtros laterais para grupo, raça e alinhamento
+    filter_horizontal = ('poderes',)  # Configuração para seleção múltipla de poderes
+
+    fieldsets = (
+        (None, {
+            'fields': ('nome', 'grupo', 'raça', 'alinhamento', 'descricao', 'foto', 'poderes')
+        }),
+    )
+
+@admin.register(Poder)
+class PoderAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'descricao')  # Exibe código e descrição do poder
+    search_fields = ('descricao',)  # Barra de pesquisa pela descrição
--- projeto/persona/views.py
+++ projeto/persona/views.py
@@ -1,16 +1,16 @@
-from django.views.generic import View
+from django.views.generic import View, ListView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from persona.models import Personagem
 from django.http import FileResponse, Http404
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from persona.models import Personagem
 from persona.form import FormularioPersona
 from persona.serializers import SerializadorPersona
 from rest_framework.generics import ListAPIView
 from rest_framework import permissions
 from rest_framework.authentication import TokenAuthentication
+from django.contrib import messages
 
 class ListarPersonagens(LoginRequiredMixin, ListView):
     """
@@ -19,6 +19,7 @@ class ListarPersonagens(LoginRequiredMixin, ListView):
     model = Personagem
     context_object_name = 'personagens'
     template_name = 'persona/listar.html'
+    paginate_by = 10  # Adiciona paginação, 10 itens por página
 
 class CriarPersonagens(LoginRequiredMixin, CreateView):
     """
@@ -29,22 +30,42 @@ class CriarPersonagens(LoginRequiredMixin, CreateView):
     template_name = 'persona/novo.html'
     success_url = reverse_lazy('listar-personagens')
 
+    def form_valid(self, form):
+        messages.success(self.request, 'Personagem criado com sucesso!')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Erro ao criar personagem. Verifique os dados informados.')
+        return super().form_invalid(form)
+
 class EditarPersonagens(LoginRequiredMixin, UpdateView):
     """
-    View para a edição de personagens já cadastrados
+    View para a edição de personagens já cadastrados.
     """
     model = Personagem
     form_class = FormularioPersona
     template_name = 'persona/editar.html'
     success_url = reverse_lazy('listar-personagens')
 
+    def form_valid(self, form):
+        messages.success(self.request, 'Personagem atualizado com sucesso!')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Erro ao atualizar personagem. Verifique os dados informados.')
+        return super().form_invalid(form)
+
 class DeletarPersonagens(LoginRequiredMixin, DeleteView):
     """
-    View para a exclusão de personagens
+    View para a exclusão de personagens.
     """
     model = Personagem
     template_name = 'persona/deletar.html'
     success_url = reverse_lazy('listar-personagens')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, 'Personagem excluído com sucesso!')
+        return super().delete(request, *args, **kwargs)
 
 class FotoPersonagem(View):
     """
@@ -54,20 +75,20 @@ class FotoPersonagem(View):
     def get(self, request, arquivo):
         try:
             persona = Personagem.objects.get(foto='persona/fotos/{}'.format(arquivo))
-            return FileResponse(persona.foto)
+            return FileResponse(persona.foto.open('rb'), content_type='image/jpeg')
         except ObjectDoesNotExist:
-            raise Http404("Foto não encontrada ou acesso não-autorizado!")
+            raise Http404("Foto não encontrada.")
         except Exception as exception:
+            messages.error(request, 'Erro ao acessar a foto do personagem.')
             raise exception
-        
+
 class APIListarPersonagens(ListAPIView):
     """
-    View para listar instancias de personagens (por meio da API Rest)
+    View para listar instâncias de personagens via API.
     """
     serializer_class = SerializadorPersona
     authentication_classes = [TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
-
 
     def get_queryset(self):
         return Personagem.objects.all()
