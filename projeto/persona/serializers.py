db640ba79e1f6092b5189bc51b66e74323080062
ACN-Destroyer <icefirethunderlight@gmail.com>
2024-11-13 16:39:59 -0400

    atualiza aula

--- projeto/persona/serializers.py
+++ projeto/persona/serializers.py
@@ -1,14 +1,15 @@
 from rest_framework import serializers
-from persona.models import Personagem
+from persona.models import Personagem, Poder
 
 
 class SerializadorPersona(serializers.ModelSerializer):
     """
-    Serializador para o model Persona
+    Serializador para o model Personagem
     """
-    nome_marca = serializers.SerializerMethodField()
-    nome_cor = serializers.SerializerMethodField()
-    nome_combustivel = serializers.SerializerMethodField()
+    nome_alinhamento = serializers.SerializerMethodField()
+    nome_grupo = serializers.SerializerMethodField()
+    nome_raca = serializers.SerializerMethodField()
+    poderes = serializers.SerializerMethodField()  # Campo para listar poderes
 
     class Meta:
         model = Personagem
@@ -22,4 +23,7 @@ class SerializadorPersona(serializers.ModelSerializer):
     
     def get_nome_raca(self, instancia):
         return instancia.get_raca_display()
-    
+
+    def get_poderes(self, instancia):
+        # Obtém os nomes dos poderes associados e os retorna como uma lista
+        return [poder.get_codigo_display() for poder in instancia.poderes.all()]
